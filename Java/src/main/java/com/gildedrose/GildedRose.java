5d2ec91394036f949b04ff9ddb95f764f2a2f296
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2019-08-10 13:35:03 +0200

    Group if statements when updating quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,12 +22,17 @@ class GildedRose {
 
     private void updateQuality(Item item) {
         if (item.name.equals("Aged Brie")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            if (item.sellIn < 0 && item.quality < 50) {
+                item.quality += 2;
+            } else if (item.quality < 50) {
+                item.quality += 1;
 
             }
         } else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
+
+            if(item.sellIn <= 0 ){
+                item.quality = 0;
+            }else if (item.quality < 50) {
                 if (item.sellIn < 6 ) {
                     item.quality += 3;
                 } else if (item.sellIn < 11 ) {
@@ -38,20 +43,11 @@ class GildedRose {
             }
         } else {
             if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        }
 
-        if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie") && item.quality < 50) {
-                item.quality = item.quality + 1;
-            } else {
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = 0;
+                if(item.sellIn < 0) {
+                    item.quality -= 2;
                 } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.quality -= 1;
                 }
             }
         }
