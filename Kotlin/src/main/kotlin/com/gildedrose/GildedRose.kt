fe4fc49249bcf13ecdb7416b9b149a24870ac41f
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2019-08-11 02:51:34 +0200

    Just for fun and my <3 for Kotlin

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,58 +1,84 @@
 package com.gildedrose
 
+import kotlin.math.max
+import kotlin.math.min
+
 class GildedRose(var items: Array<Item>) {
-
     fun updateQuality() {
-        for (i in items.indices) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
-        }
+        items.forEach { InventoryItem.from(it).ageOneDay() }
     }
+}
+
+
+sealed class InventoryItem(val item: Item) {
+    open fun ageOneDay() {
+        item.sellIn = item.decreaseSellIn()
+        item.quality = calculateQuality()
+    }
+
+    abstract fun calculateQuality(): Int
+
+    companion object {
+        fun from(item: Item): InventoryItem =
+                when (item.name) {
+                    "Aged Brie" -> AgedBrie(item)
+                    "Sulfuras, Hand of Ragnaros" -> LegendaryItem(item)
+                    "Backstage passes to a TAFKAL80ETC concert" -> BackStagePass(item)
+                    "Conjured Mana Cake" -> ConjuredItem(item)
+                    else -> NormalItem(item)
+                }
+    }
+}
+
+class AgedBrie(item: Item) : InventoryItem(item) {
+    override fun calculateQuality(): Int =
+            when {
+                item.sellIn < 0 -> item.increaseQualityBy(2)
+                else -> item.increaseQualityBy(1)
+            }
 
 }
 
+class BackStagePass(item: Item) : InventoryItem(item) {
+    override fun calculateQuality(): Int =
+            when {
+                item.sellIn < 0 -> 0
+                item.sellIn < 6 -> item.increaseQualityBy(3)
+                item.sellIn < 11 -> item.increaseQualityBy(2)
+                else -> item.increaseQualityBy(1)
+            }
+
+}
+
+class LegendaryItem(item: Item) : InventoryItem(item) {
+    override fun ageOneDay() {}
+    override fun calculateQuality(): Int = 0
+}
+
+class NormalItem(item: Item) : InventoryItem(item) {
+    override fun calculateQuality(): Int =
+            when {
+                item.quality <= 0 -> 0
+                item.sellIn < 0 -> item.decreaseQualityBy(2)
+                else -> item.decreaseQualityBy(1)
+            }
+
+}
+
+class ConjuredItem(item: Item) : InventoryItem(item) {
+    override fun calculateQuality(): Int =
+            when {
+                item.quality <= 0 -> 0
+                item.sellIn < 0 -> item.decreaseQualityBy(4)
+                else -> item.decreaseQualityBy(2)
+            }
+}
+
+
+private fun Item.decreaseSellIn() = this.sellIn - 1
+
+private fun Item.increaseQualityBy(value: Int, max: Int = 50) =
+        min(this.quality + value, max)
+
+private fun Item.decreaseQualityBy(value: Int, min: Int = 0) =
+        max(this.quality - value, min)
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,18 +1,50 @@
 package com.gildedrose
 
-import org.junit.Assert.*
-import org.junit.Test
+import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+    @ParameterizedTest(name = "Given: An item \"{0}\" with sellIn \"{1}\" and quality \"{2}\" When: quality is updated Then: sellIn should be \"{3}\" And quality should be \"{4}\"")
+    @CsvSource(delimiter = '|', value = [
+        //                                     name | sellin | quality | expected sellin | expected quality
+        "                         +5 Dexterity Vest |     10 |      20 |               9 |               19 ",
+        "                         +5 Dexterity Vest |      0 |      20 |              -1 |               18 ",
+        "                         +5 Dexterity Vest |     10 |       0 |               9 |                0 ",
+        "                         +5 Dexterity Vest |     -1 |       0 |              -2 |                0 ",
+        "                         +5 Dexterity Vest |     10 |      60 |               9 |               59 ",
+        "                                 Aged Brie |     10 |      10 |               9 |               11 ",
+        "                                 Aged Brie |     10 |      50 |               9 |               50 ",
+        "                                 Aged Brie |     -5 |      10 |              -6 |               12 ",
+        "                Sulfuras, Hand of Ragnaros |     10 |      80 |              10 |               80 ",
+        "                Sulfuras, Hand of Ragnaros |     -1 |      80 |              -1 |               80 ",
+        "                Sulfuras, Hand of Ragnaros |      9 |      60 |               9 |               60 ",
+        " Backstage passes to a TAFKAL80ETC concert |     20 |      20 |              19 |               21 ",
+        " Backstage passes to a TAFKAL80ETC concert |     10 |      20 |               9 |               22 ",
+        " Backstage passes to a TAFKAL80ETC concert |      9 |      20 |               8 |               22 ",
+        " Backstage passes to a TAFKAL80ETC concert |      5 |      20 |               4 |               23 ",
+        " Backstage passes to a TAFKAL80ETC concert |     15 |      50 |              14 |               50 ",
+        " Backstage passes to a TAFKAL80ETC concert |     10 |      49 |               9 |               50 ",
+        " Backstage passes to a TAFKAL80ETC concert |      5 |      48 |               4 |               50 ",
+        " Backstage passes to a TAFKAL80ETC concert |      0 |      20 |              -1 |                0 ",
+        "                        Conjured Mana Cake |     10 |      20 |               9 |               18 ",
+        "                        Conjured Mana Cake |     -1 |      20 |              -2 |               16 ",
+        "                        Conjured Mana Cake |     10 |       1 |               9 |                0 "])
+    fun `that an items quality is updated as expected when item ages by one day`(name: String, sellIn: Int, quality: Int, expectedSellIn: Int, expectedQuality: Int) {
 
+        //Given:
+        val item = Item(name, sellIn, quality)
+
+        //When:
+        GildedRose(arrayOf(item)).updateQuality()
+
+        //Then:
+        assertSoftly { softly ->
+            softly.assertThat(item.sellIn).isEqualTo(expectedSellIn)
+            softly.assertThat(item.quality).isEqualTo(expectedQuality)
+        }
     }
-
 
 }
 
